9d2f267b763041633eeddece01afeace41965b73
erica007 <erica777erica@gmail.com>
2022-09-24 11:39:03 -0700

    Add infobox for the photo gallery

--- js/java.js
+++ js/java.js
@@ -1,8 +1,8 @@
 // Page 4 Gallery Script
-// loop for image gallery
-var captionTexts = ["Victor, from local", "Teddy, from local", "King, from local",
+// image gallery
+var captionTexts = ["Petty, from local", "Chao, from local", "King, from local",
                         "Golden, from local", "Joy, from local"
-                        ,"Memes, in foster family" , "Buddies, traveling", "Greys, from AZ", 
+                        ,"Triples, in foster family" , "Buddies, traveling", "Greys, from AZ", 
                         "Cathy, in foster family", "smily, condition adoption" ,"Mister lonely, from local"];
 var descTexts = ["12 months", "6 months","18 months", "5 months","16 months", "7 months","10 months", 
 "6 months","12 months", "6 months","3 months"];
@@ -17,12 +17,25 @@ var openList = "<li class='photo'>";
 var closeList = "</li>";
 var openCaptionTag = "<p class='caption'>";
 var closeCaptionTag = "</p>";
-var titleTexts = ["Dog - Vince", "Dog - Teddy", "Dog - Victor", "Dog - Uni", "Dog - Meatball"
-                    , "Cat - Meme", "Cat - Flur", "Cat - Lia", "Cat - Cath", "Cat - CC"];
-var openDescTag = "<p class='description' (";
+var titleTexts = ["British Siliver","Domestic Shorthair","Abyssinian","Goldendoddle","golden retriever",
+                  "Guinea pig","Domestic Shorthair Cat & Mixed Dog", "British Grey","golden retriever",
+                  "Guinea pig","Burmilla"];
+var infoTexts = ["We resued Petty when she was 2 months old, she is not sterilized yet but it's required for adoption."
+                , "Chao does not like kids,sterilized already. Need family with backyard."
+                , "King does not get along with other cats (Spoiled urh?), require family with no cats, dog is fine."
+                , "Godlen gets along with kids and bacially all creatures on earth."
+                , "Joy has knee problem and need to do surgery."
+                , "Triples need to be adopted together."
+                , "Buddies need to be adopted togehter."
+                , "Greys need to be adopted togehter."
+                , "Cathy is a good baby sister."
+                , "Smily is scared of dog, no dog in the house."
+                , "Mister requires experienced parents since he is just a baby."];
+var closeinfoBox = "Click to close";
+var openDescTag = "<p class='description' onClick='showinfoBox(";
 var openDescClose = ")'>";
 var closeDescTag = "</p>";
-var infoTexts = []
+
 
 //Create a loop to create 10 images
 for (var i=0; i<11; i++) {
@@ -40,6 +53,19 @@ for (var i=0; i<11; i++) {
 document.getElementById("photos").innerHTML = imageList.join(" ");
 
 
-// start infobox
+//Week 4 Day 3 Lab - infobox
+function showinfoBox(i) {
+    document.getElementById("infoBox").style.visibility = 'visible';
+    document.getElementById("title").innerHTML = titleTexts[i];
+    document.getElementById("info").innerHTML = infoTexts[i];
+    document.getElementById("closeInfo").innerHTML = closeinfoBox;
+};
 
-//  End photo gallery js
+//
+var closeInfo = document.getElementById("closeInfo");
+closeInfo.addEventListener("click", hideinfoBox);
+
+function hideinfoBox() {
+    document.getElementById("infoBox").style.visibility = 'hidden';
+};
+
